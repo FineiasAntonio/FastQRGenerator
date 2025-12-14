835bf9227b6d619e563cbb363f25843f75e9caed
FineiasAntonio <fineias1187@gmail.com>
2025-12-14 17:31:02 -0300

    Polynomial class

--- src/main/java/com/qrlib/App.java
+++ src/main/java/com/qrlib/App.java
@@ -2,8 +2,14 @@ package com.qrlib;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Polynomial p = new Polynomial(new double[]{2, 0, -3});
+        Polynomial p2 = new Polynomial(new double[]{2, 0, 3});
+
+        System.out.println("Polynomial: " + p);
+        System.out.println("p(2) = " + p.evaluate(2));
+
+        Polynomial sum = p.add(p2);
+        System.out.println("Sum: " + sum);
     }
 }
